e7e1fd8880cce5b1d4cb74a4d046b4ca334032f6
jayPo27 <75405671+jayPo27@users.noreply.github.com>
2021-06-24 16:16:20 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,21 +1,26 @@
 let color = '#3aa757';
-
+let bookURL;
 chrome.runtime.onInstalled.addListener(()=>{
 	chrome.storage.sync.set({color});
 	console.log('Default background color set to %cgreen', 'color: ${color}');
 });
+
+
 chrome.runtime.onMessage.addListener(
 	function(message, sender, sendResponse){
-		//console.log(message);
+
+		// we know it's from a popup
 		if(message.greeting === "GetURL"){
-			chrome.tabs.query({active:true}, function(tabs)){
-				if(tabs.length === 0) {
-                	sendResponse({});
-                	return;
-            	}
-            	tabURL = tabs[0].url;
-            	sendResponse( {navURL:tabURL} );
-			};
+
+			// gives you the current tab
+		let currTab = chrome.tabs.query({active:true, currentWindow: true})
+
+            	let tabURL = currTab.url;
+            	sendResponse(bookURL);
+            	//sendResponse( {siteURL:tabURL} );
 		}
+		else if(message.greeting === "GRPage"){
+			bookURL = message;
+		}	
 });
 
