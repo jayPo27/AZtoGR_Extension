86ec6922592de1854b77737915a821f805d95fda
jayPo27 <75405671+jayPo27@users.noreply.github.com>
2021-06-24 16:42:31 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,26 +1,56 @@
 let color = '#3aa757';
-let bookURL;
+let bookURL = "hello;"
 chrome.runtime.onInstalled.addListener(()=>{
 	chrome.storage.sync.set({color});
 	console.log('Default background color set to %cgreen', 'color: ${color}');
 });
 
+window.onload = () => {
+	if (window.location.href.includes('amazon.com')){
+		alert("hey i am at amazon")
+		let allSpan = document.getElementsByTagName('span')
+			for (var i=0;i<allSpan.length;i++){
+			    if(allSpan[i].innerText == "Print length"){
+			    	alert("hey I am Jay!")
+			    	let pageTitle = document.getElementsByTagName('title')
+			    	for(var j = 0; j < pageTitle.length; j++){
+			    		let str1 = pageTitle[j].innerText
+			    		var x = str1.indexOf(":");
+			    		var y = str1.substr(0,x);
+			    		var split = y.split(" ");
+			    		var str2 = "https://www.goodreads.com/search?utf8=%E2%9C%93&query="
+			    		for(var a = 0; a < split.length; a++){
+			    			str2 = str2.concat(split[a]);
+			    			if(a != split.length-1){
+			    				str2 = str2.concat('+');
+			    			}
+			    		}
+			            //chrome.runtime.sendMessage(str2, {greeting: "GRPage"});
+			    	}
+			    }
+			}
+	}
+// + get book title logic
+
+
+			
+}
 
 chrome.runtime.onMessage.addListener(
 	function(message, sender, sendResponse){
 
-		// we know it's from a popup
-		if(message.greeting === "GetURL"){
+		// // we know it's from a popup
+		// if(message.greeting === "GetURL"){
 
-			// gives you the current tab
-		let currTab = chrome.tabs.query({active:true, currentWindow: true})
+		// 	// gives you the current tab
+		// //let currTab = chrome.tabs.query({active:true, currentWindow: true});
 
-            	let tabURL = currTab.url;
-            	sendResponse(bookURL);
-            	//sendResponse( {siteURL:tabURL} );
-		}
-		else if(message.greeting === "GRPage"){
-			bookURL = message;
-		}	
+  //           	//let tabURL = currTab.url;
+  //          //sendResponse(str2);
+  //           	//sendResponse( {siteURL:tabURL} );
+		// }
+		// else if(message.greeting === "GRPage"){
+		// 	bookURL = message;
+		// }	
 });
 
