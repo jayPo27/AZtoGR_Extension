d717403990779c4752fd12f54036a9f597b55acc
Vivek Bhookya <v.bhookya98@gmail.com>
2021-06-24 16:53:59 -0500

    Content script is back in action. Goodreads query URL is incorrect tho

--- contentScript.js
+++ contentScript.js
@@ -1,23 +1,21 @@
-alert('hi')
-
-// let allSpan = document.getElementsByTagName('span')
-// for (var i=0;i<allSpan.length;i++){
-//     if(allSpan[i].innerText == "Print length"){
-//     	alert("hey I am Jay!")
-//     	let pageTitle = document.getElementsByTagName('title')
-//     	for(var j = 0; j < pageTitle.length; j++){
-//     		let str1 = pageTitle[j].innerText
-//     		var x = str1.indexOf(":");
-//     		var y = str1.substr(0,x);
-//     		var split = y.split(" ");
-//     		var str2 = "https://www.goodreads.com/search?utf8=%E2%9C%93&query="
-//     		for(var a = 0; a < split.length; a++){
-//     			str2 = str2.concat(split[a]);
-//     			if(a != split.length-1){
-//     				str2 = str2.concat('+');
-//     			}
-//     		}
-//             chrome.runtime.sendMessage(str2);
-//     	}
-//     }
-// }
+let allSpan = document.getElementsByTagName('span')
+for (var i=0;i<allSpan.length;i++){
+    if(allSpan[i].innerText == "Print length"){
+    	// alert("hey I am Jay!")
+    	let pageTitle = document.getElementsByTagName('title')
+    	for(var j = 0; j < pageTitle.length; j++){
+    		let str1 = pageTitle[j].innerText
+    		var x = str1.indexOf(":");
+    		var y = str1.substr(0,x);
+    		var split = y.split(" ");
+    		var str2 = "https://www.goodreads.com/search?utf8=%E2%9C%93&query="
+    		for(var a = 0; a < split.length; a++){
+    			str2 = str2.concat(split[a]);
+    			if(a != split.length-1){
+    				str2 = str2.concat('+');
+    			}
+    		}
+            alert(str2);
+    	}
+    }
+}
