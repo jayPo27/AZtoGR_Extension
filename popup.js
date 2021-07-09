b3a9d1e05c08d78c3636e086ccf1f6b067536440
jayPo27 <75405671+jayPo27@users.noreply.github.com>
2021-07-09 12:24:02 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -5,6 +5,21 @@ function toGR(){
 }
 
 
-document.getElementById("button1").addEventListener("click", function() {
-	toGR();
+function getCurrentISBN(id){
+	chrome.tabs.sendMessage(id.id, {"message": "ISBN"});
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+	document.getElementById("button1").addEventListener("click", function() {
+		chrome.tabs.query({active: true, currentWindow: true}, tabs =>{
+			let url = tabs[0].url;
+			if(url.includes('amazon.com')){
+				getCurrentISBN(tabs[0]);
+				//toGR();
+			}
+			else{
+				alert("This extension only works on Amazon");
+			}
+		})	
+	});
 });
